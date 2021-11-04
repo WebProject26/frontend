ff614301e05db46e82783d6cb623d0d7624ed592
SIF\alrai <al.rainov@gmail.com>
2021-11-04 15:17:57 +0200

    commit before branching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import styles from './App.module.css';
+//import axios from './axios';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+     
+    }
+  }
+/*
+  componentDidMount() {
+    axios.get('http://localhost:3030/products')
+      .then(response => this.setState( { products: response.data }))
+      .catch(error => console.log(error))
+
+    axios.get('http://localhost:3030/cart')
+         .then(response => this.setState( { cart: response.data } ))
+         .catch(error => console.log(error))
+  }
+  */
+
+  render() {
+    return (
+      <div className= { styles.App }>
+
+      </div>
+    )
+  }
 }
 
 export default App;
