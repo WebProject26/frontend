e10c82628e7566cb15b7f9fa30b3afa2dd5c3e3b
SIF\alrai <al.rainov@gmail.com>
2021-11-04 11:00:58 +0200

    still testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import axios from 'axios';
 
-class App extends React.Component {
-  constructor(props)
-  {
-    super(props)
-
-    this.state = {
-      sdsd
-      sdsdd
-    }
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3030/products')
-      .then(response => this.setState( { products: response.data }))
-      .catch(error => console.log(error))
-
-    axios.get('http://localhost:3030/cart')
-         .then(response => this.setState( { cart: response.data } ))
-         .catch(error => console.log(error))
-  }
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
 }
 
 export default App;
