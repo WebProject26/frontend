df8c6d0efdd1db26ff6461a5308773ca164b11ea
SIF\alrai <al.rainov@gmail.com>
2021-11-04 10:59:12 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+      sdsd
+      sdsdd
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3030/products')
+      .then(response => this.setState( { products: response.data }))
+      .catch(error => console.log(error))
+
+    axios.get('http://localhost:3030/cart')
+         .then(response => this.setState( { cart: response.data } ))
+         .catch(error => console.log(error))
+  }
 }
 
 export default App;
